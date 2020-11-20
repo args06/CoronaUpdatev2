c1f4db62c9a88cc4a3aca9069d6fa6cb05b8a433
args06 <acool90@yahoo.co.id>
2020-11-20 14:25:21 +0700

    Gradle Update

--- app/src/main/java/com/example/coronaupdate/SplashActivity.java
+++ app/src/main/java/com/example/coronaupdate/SplashActivity.java
@@ -8,20 +8,17 @@ import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_UOT = 3000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIME_UOT);
+        new Handler().postDelayed(() -> {
+            Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }, SPLASH_TIME_OUT);
     }
 }
